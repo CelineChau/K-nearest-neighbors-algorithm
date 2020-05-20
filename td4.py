4f689b4369144850ec149e61e29a50990f2c56df
CHAU Céline <littlecelinec@gmail.com>
2020-05-20 09:52:47 +0200

    Simple Algo Knn + output test

--- td4.py
+++ td4.py
@@ -10,59 +10,103 @@ Created on Wed Apr 29 16:15:40 2020
 
 import numpy as np
 import pandas as pd
+
 # Apprentissage supervisé - k plus proches voisins
 
 # param modifiant le comportement de l'algo : k, notion de proche, arbitrer classification
 # algo en régression : Donner une valeur à chaque classe (moyenne)
 
-# Distance Euclidienne 
-def distance_euclidienne(a, b) :
+# Load data into dataset 
+def readData(csv_data_set_path, csv_data_test_path = None) :
+    data_set = []
+    data_test = []
+    labels = []
+    sublabels = []
+    
+    # Load data set
+    df1 = pd.read_csv(csv_data_set_path, header=None, sep=";")
+    #df1.columns = names
+    data = df1.to_numpy()
+    
+    if csv_data_test_path is None :
+        raw_set_number = round(df1.shape[0] * 0.8)
+        data_set = data[0:raw_set_number, 0:4]
+        data_test = data[raw_set_number:, 0:4]
+        sublabels = data[raw_set_number:, -1]
+        labels = data[0:raw_set_number, -1]
+    else :
+        data_set = data[:,0:4]
+        df2 = pd.read_csv(csv_data_test_path, header=None, sep=";") 
+        data_test = df2.to_numpy()
+        labels = data[:, -1]
+        
+    return data_set, data_test, labels, sublabels
+
+# Get first element of each sublist 
+def extract(lst): 
+    return [item[0] for item in lst] 
+
+# Find most frequent element in a list
+def most_frequent(lst): 
+    return max(set(lst), key = lst.count) 
+
+# Euclidien distance
+def euclidean_distance(a, b) :
     return np.sqrt(np.sum((a - b)**2))
 
-# test : Distance Euclidienne
-a = np.random.randint(10, size=2)
-b = np.random.randint(10, size=2)
-print("a : ",a, " b :", b, " distance euclidienne : ", distance_euclidienne(a,b))
-
-# Charge data into dataset 
-def readData(csv_path, names) :
-    df = pd.read_csv(csv_path)
-    df.columns = names
-    return df
-   
-# Vote des k plus proches voisins dans l'ensemble d'entraînement
-
-
-""" 
-    Main function  : k plus proche
-    params :
-        - X : entrée à classifier
-        - k : nb entrées plus proches de X
-        - data : exemples d'apprentissage
-"""
-def k_plus_proche(X, k, data) :
-    distances = dict()
-    k_entrees = []
-    # Trouver les k plus proches
+# k-nearest algorithm
+def k_plus_proche(data_set, labels, X, K) :
+    list_dist = []
+    selected_labels = []
     
-    # Calculate distances
-    for i in range(len(data)) :  
-        distances[distance_euclidienne(X, data[i])] = data[i]
+    # For each entree in the data
+    for i, entree in enumerate(data_set) :
+        # Calculate distance between the given example and the current example 
+        dist = euclidean_distance(entree, X)
+        # Add distance and index of the example to an ordered collection
+        list_dist.append([i, dist])
+        
+    # Sort the list of distances and indices by distance ASC
+    list_dist.sort(key=lambda x: x[1])
     
-    print("\n distances : ", distances)
+    # Pick the first K entries from the sorted list
+    neighbors = list_dist[:K]
     
-    # Faire voter les k
+    # Get the labels of the selected K neighbors
+    for index in extract(neighbors) :
+        selected_labels.append(labels[index])
     
-    # Retourner classe majoritaire
+    # Return the mode of the K labels
+    return most_frequent(selected_labels)
 
-################################ Exemple Iris ################################
+def main() :
+    # Load data
+    #percentage = 0
+    data_set, data_test, labels, sublabels = readData("data.csv", "finalTest.csv")
+#    k = int(round(np.sqrt(len(data_set))))
+    k = 10
+    print("K", k)
+    
+    f = open("CHAU_NORMAND.txt", "w")
+    
+    for i, x in enumerate(data_test) :
+        # Format noms et solution
+        res = k_plus_proche(data_set, labels, x, k)
+        # Ecriture dans un fichier
+#        if res == sublabels[i] :
+#            percentage += 1
+        f.write(res + "\n")
+        
+    f.close()
 
-#Charge
-df = readData("iris.data.csv", ["Sepal length", "Sepal width", "Petal length", "Petal width", "class label"])
-print(df)
+    
 
-# Récupérer données d'apprentissage
-data = df.to_numpy()
-print("\n data : ", data)
-#X = np.random.randint(10, size=3)
-#k_plus_proche(X, 2, data)
+if __name__ == "__main__":
+    
+    main()
+    
+
+
+
+
+
